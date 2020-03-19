d6d677657e4412e73c60d91975b457c93034dce1
Fabio Arpires <arpirez.fabio@gmail.com>
2020-03-19 18:54:53 -0300

    Angular: Multiples o  varios Componentes

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videoJuego/videoJuego.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 
 @NgModule({
   declarations: [//aqui van las directivas componentes
     AppComponent,
-    VideojuegoComponent
+    VideojuegoComponent,
+    ZapatillasComponent
 
   ],
   imports: [
--- aprendiendo-angular/src/app/videoJuego/videoJuego.component.ts
+++ aprendiendo-angular/src/app/videoJuego/videoJuego.component.ts
@@ -5,18 +5,17 @@ import { Component } from '@angular/core';
     //nombre de la etiqueta o nombre del componente
     selector: 'videojuego', 
    //una plantilla en linea
-    template: `
-    <h2> Componente de Video Juegos <h2>
-    <ul>
-    <li>GTA</li>
-    <li>Prince of Persia</li>
-    <li>Teken</li> 
-    <li>Mario</li>
-    </ul>
-    ` 
+    // template: ` `
+    templateUrl:'./videojuego.component.html' 
 })
 export class VideojuegoComponent {
-      constructor(){
+    
+    public titulo: string;//propiedades
+    public listado: string;
+
+      constructor(){//se le da valor a alas propiedades
+          this.titulo = 'Componentde de Video Juegos2';
+          this.listado= 'Listadod e los juegos mas populares2020';
           console.log('se ha cargado el compnent: videojuego.componnetn.ts');
       }
 }
